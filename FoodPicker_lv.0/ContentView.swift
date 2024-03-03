2d80dbb1fc3137260f53f4563e61cc73d203a9aa
jerome_Hsieh <jerome910810@gmail.com>
2024-03-04 00:23:03 +0800

    Initial Commit

---
+++ FoodPicker_lv.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodPicker_lv.0
+//
+//  Created by HsinYuan on 2024/3/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodPicker_lv.0/FoodPicker_lv_0App.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPicker_lv_0App.swift
+//  FoodPicker_lv.0
+//
+//  Created by HsinYuan on 2024/3/4.
+//
+
+import SwiftUI
+
+@main
+struct FoodPicker_lv_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
